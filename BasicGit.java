847af0681a12453d1e5bb30051bbfcba915e76b6
Josh Rodstein <joshuarodstein@gmail.com>
2017-10-16 15:34:07 -0400

    final

---
+++ BasicGit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class BasicGit {
+
+    public static void main(String[] args){
+	for(int i = 0; i < 10; i++)
+	    System.out.println(i+1);
+
+
+	System.exit(0);
+    }
+}
